c70346028ae066e3f81b82119672a2b7bd7d97fa
Ali Neaz <neazali77@gmail.com>
2021-07-27 01:09:38 +0600

    changed the file formate

---
+++ 02_slice.py
@@ -0,0 +1,27 @@
+# name = "Ali Neaz"
+# print(name)
+# print(type(name))  # class str type string
+# greeting = "Good Morning"
+# c = greeting+" "+name
+# print(c)
+name = "Ali Neaz"
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+# print(name[0:3])
+# print(name[10])  # invalid cause lenth is 8 and it exceds 8
+# print(name[0:10])
+# print(len(name))
+# name[4] = "A"  # cant be done this not changeable
+# print(name)
+# _______________until the lenth the [] doesnot includ the last value
+# print(name[0:3])  # will pring 0 1 2 indeces
+# print(name[-1])
+# print(name[-6])  # index -5 is white space
+# print(name[-8])
+
+
+print(name[-1:-2])  # not working or garage wrong method
+print(name[:8])  # first index will automatically be 0 though i havent specified
+print(name[0:])  # first is specified last will be then lenth
--- problems/02_slice.py
+++
@@ -1,6 +0,0 @@
-name = "Ali Neaz"
-# print(name)
-# print(type(name))  # class str type string
-greeting = "Good Morning"
-c = greeting+" "+name
-print(c)
